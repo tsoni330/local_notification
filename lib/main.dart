dee018c810be0f7945b17e5ce0dcbfa0621415f2
Tushar Soni <tusharsoni330gmail.com>
2023-10-11 12:34:45 +0530

    Large text size

--- lib/main.dart
+++ lib/main.dart
@@ -113,19 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: Text("Tap on floating Action Button",style: Theme.of(context).textTheme.headlineSmall,),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: showNotification,
